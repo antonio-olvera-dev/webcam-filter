c859e558b6b824eb36b000449070fcef7b07651e
Antonio Olvera <antonioolvera1995@gmail.com>
2021-12-12 20:41:44 +0100

    update Description

--- src/core/home/components/description/Description.js
+++ src/core/home/components/description/Description.js
@@ -2,10 +2,9 @@ import './Description.css';
 export default function Description(props) {
     return (
         <div className='description'>
-            <h1>Home</h1>
+            <h1>Webcam filter</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
-                voluptatum.
+                This is a small project that, based on a color and range, looks for those pixels on the canvas and replaces them with a given color. It also separates those searched pixels and separates them into groups based on the given range.
             </p>
         </div>
     )
